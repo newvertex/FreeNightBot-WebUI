d5803e858394dec044d1083b7b97f63002de8fad
Mojtaba Rabiei <mojtaba.r127@gmail.com>
2017-02-12 14:18:35 +0330

    Add Help component with dummy text

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 
 import Home from './components/home/Home';
+import Help from './components/help/Help';
 
 import { tmpBrand, tmpUser } from './config';
 
@@ -13,8 +14,8 @@ class App extends Component {
     return (
       <div className='main-wrapper'>
         <Header brand={tmpBrand} user={tmpUser} />
-        <div className='main-content'>
-          <Home />
+        <div className='main-content v-align'>
+          <Help />
         </div>
         <Footer brand={tmpBrand} />
       </div>
